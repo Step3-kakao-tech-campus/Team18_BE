a6e2a1bf1bc00514ef4099adab5cb5aa266f52d1
Jin_00 <anjdal64@gmail.com>
2023-10-04 09:29:09 +0900

    feat : 예시 기능 작성

--- src/main/java/com/example/demo/account/AccountRestController.java
+++ src/main/java/com/example/demo/account/AccountRestController.java
@@ -1,6 +1,7 @@
 package com.example.demo.account;
 
 import com.example.demo.config.utils.ApiUtils;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,7 @@ public class AccountRestController {
     private final AccountService accountService;
 
     @PostMapping(value = "/users/signup")
+    @Operation(summary = "회원가입", description = "SignUpDTO 를 받아서 회원가입 진행")
     public ResponseEntity<?> signup(@RequestPart @Valid AccountRequest.SignUpDTO requestDTO, Errors errors) {
         accountService.signup(requestDTO);
         return ResponseEntity.ok().body(ApiUtils.success(true));
