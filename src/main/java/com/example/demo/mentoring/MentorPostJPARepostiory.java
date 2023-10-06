237c46349e83c6a4f66c4739c24ce6184671dd66
Jin_00 <anjdal64@gmail.com>
2023-10-06 21:15:36 +0900

    fix : 쿼리문 수정

--- src/main/java/com/example/demo/mentoring/MentorPostJPARepostiory.java
+++ src/main/java/com/example/demo/mentoring/MentorPostJPARepostiory.java
@@ -1,11 +1,15 @@
 package com.example.demo.mentoring;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface MentorPostJPARepostiory extends JpaRepository<MentorPost, Integer> {
-    List<MentorPost> findAllByWriter(int writer);
+
+    @Query("select m from MentorPost m where m.writer.id = :writer")
+    List<MentorPost> findAllByWriter(@Param("writer") int writer);
 
     MentorPost findById(int id);
 }
