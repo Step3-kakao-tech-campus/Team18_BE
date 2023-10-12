3d525011ab677cad611b89545d46296b54bea857
Jin_00 <anjdal64@gmail.com>
2023-10-12 22:19:41 +0900

    test : /contacts, /contacts/countPosts API Test

--- src/main/resources/db/teardown.sql
+++ src/main/resources/db/teardown.sql
@@ -4,6 +4,7 @@ TRUNCATE TABLE interest_tb;
 TRUNCATE TABLE mentor_post_tb;
 TRUNCATE TABLE user_interest_tb;
 TRUNCATE TABLE not_connected_register_user_tb;
+TRUNCATE TABLE connected_user_tb;
 SET REFERENTIAL_INTEGRITY TRUE;
 
 -- user Table
@@ -12,7 +13,8 @@ INSERT INTO user_tb (created_at, first_name, last_name, email, password, country
    (NOW(), 'Alice', 'Smith', 'alice.smith@example.com', '{bcrypt}$2a$10$8H0OT8wgtALJkig6fmypi.Y7jzI5Y7W9PGgRKqnVeS2cLWGifwHF2', 'Canada', 'I love painting.', 25, 'image2.jpg', 'MENTOR'),
    (NOW(), 'Admin', 'Admin', 'admin@example.com', '{bcrypt}$2a$10$8H0OT8wgtALJkig6fmypi.Y7jzI5Y7W9PGgRKqnVeS2cLWGifwHF2', 'USA', 'I am an admin user.', 35, 'admin.jpg', 'MENTEE'),
    (NOW(), 'Jane', 'Smith', 'jane@example.com', '{bcrypt}$2a$10$8H0OT8wgtALJkig6fmypi.Y7jzI5Y7W9PGgRKqnVeS2cLWGifwHF2', 'Canada', 'I love coding.', 30, 'profile.jpg', 'MENTEE'),
-   (NOW(), 'Admin', 'Adminson', 'admin22222@example.com', '{bcrypt}$2a$10$8H0OT8wgtALJkig6fmypi.Y7jzI5Y7W9PGgRKqnVeS2cLWGifwHF2', 'USA', 'I am the admin.', 35, 'admin.jpg', 'MENTEE');
+   (NOW(), 'Admin', 'Adminson', 'admin22222@example.com', '{bcrypt}$2a$10$8H0OT8wgtALJkig6fmypi.Y7jzI5Y7W9PGgRKqnVeS2cLWGifwHF2', 'USA', 'I am the admin.', 35, 'admin.jpg', 'MENTEE'),
+   (NOW(), 'Adminnnn', 'Adminsonnnnn', 'admin33333@example.com', '{bcrypt}$2a$10$8H0OT8wgtALJkig6fmypi.Y7jzI5Y7W9PGgRKqnVeS2cLWGifwHF2', 'USA', 'I am the admin.', 35, 'admin.jpg', 'MENTEE');
 
 -- interst Table
 INSERT INTO interest_tb (created_at, category) VALUES
@@ -36,7 +38,7 @@ INSERT INTO mentor_post_tb (created_at, writer_id, title, content) VALUES
       (NOW(), 1, 'Graphic Design Mentorship', 'Create stunning graphics and designs.'),
       (NOW(), 1, 'Yoga and Mindfulness Mentorship', 'Find inner peace and balance through yoga.'),
       (NOW(), 1, 'Photography Mentorship', 'Capture amazing moments with your camera.'),
-      (NOW(), 2, 'Mathematics Tutoring', 'I can help you understand and excel in math.');;
+      (NOW(), 2, 'Mathematics Tutoring', 'I can help you understand and excel in math.');
 
 
 -- notConnectedRegisterUser Table
@@ -44,6 +46,13 @@ INSERT INTO not_connected_register_user_tb (created_at, mentor_post_id, mentee_u
       (NOW(), 1, 3, 'AWAIT'),
       (NOW(), 1, 4, 'ACCEPT'),
       (NOW(), 2, 5, 'AWAIT');
+
+-- connectedUser Table
+INSERT INTO connected_user_tb (created_at, mentor_user_id, mentee_user_id)
+VALUES
+    (NOW(), 1, 5),
+    (NOW(), 1, 6),
+    (NOW(), 2, 3);
 
 -- userInterest Table
 INSERT INTO user_interest_tb (created_at, user_id, interest_id) VALUES
@@ -56,5 +65,6 @@ INSERT INTO user_interest_tb (created_at, user_id, interest_id) VALUES
    (NOW(), 4, 1),
    (NOW(), 4, 3),
    (NOW(), 5, 3),
-   (NOW(), 5, 4)
-;
+   (NOW(), 5, 4),
+   (NOW(), 5, 3),
+   (NOW(), 5, 4);
