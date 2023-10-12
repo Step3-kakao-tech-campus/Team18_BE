4b92acfe9379e1cbede1e7ce975592cb56a23fae
Jin_00 <anjdal64@gmail.com>
2023-10-12 22:21:52 +0900

    feat : /contacts mentor, mentee 화면 분리

--- src/main/java/com/example/demo/mentoring/contact/ContactRestController.java
+++ src/main/java/com/example/demo/mentoring/contact/ContactRestController.java
@@ -21,9 +21,14 @@ public class ContactRestController {
     @GetMapping(value = "/contacts")
     @Operation(summary = "", description = "")
     public ResponseEntity<?> findAll(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        List<ContactResponse.MentorPostDTO> responseDTO = contactService.findAll(userDetails.getUser().getId());
+        if ( userDetails.getUser().getRole() == Role.MENTEE ) {
+            List<ContactResponse.MenteeContactDTO> responseDTO = contactService.findAllByMentee(userDetails.getUser());
+            return ResponseEntity.ok(ApiUtils.success(responseDTO));
+        }
+        List<ContactResponse.MentorPostDTO> responseDTO = contactService.findAllByMentor(userDetails.getUser());
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
+}
     
     @GetMapping(value = "/contacts/postCounts")
     @Operation(summary = "", description = "")
