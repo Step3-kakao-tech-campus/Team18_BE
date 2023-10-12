1f3931d3e2472551e9526a78a84399643cf54415
Jin_00 <anjdal64@gmail.com>
2023-10-12 23:06:39 +0900

    feat : connectedUser table 에 더미데이터 추가

--- src/main/resources/db/teardown.sql
+++ src/main/resources/db/teardown.sql
@@ -24,35 +24,34 @@ INSERT INTO interest_tb (created_at, category) VALUES
   (NOW(), 'Sports');
 
 -- mentorPost Table
-INSERT INTO mentor_post_tb (created_at, writer_id, title, content) VALUES
-      (NOW(), 1, 'Teaching Programming', 'I can teach you how to code.'),
-      (NOW(), 1, 'Art Workshop', 'Let''s create beautiful art together.'),
-      (NOW(), 2, 'Software Development Mentorship', 'I can mentor you in software development.'),
-      (NOW(), 2, 'Art and Painting Mentorship', 'Learn the art of painting with me.'),
-      (NOW(), 1, 'Web Development Mentorship', 'I can teach you web development from scratch.'),
-      (NOW(), 2, 'Fitness and Health Mentorship', 'Get in shape and stay healthy with my guidance.'),
-      (NOW(), 1, 'Data Science Mentorship', 'Learn data science and machine learning with me.'),
-      (NOW(), 2, 'Music Production Mentorship', 'Produce your own music with professional tips.'),
-      (NOW(), 2, 'Cooking and Culinary Arts Mentorship', 'Master the art of cooking and culinary skills.'),
-      (NOW(), 2, 'Entrepreneurship Mentorship', 'Start and grow your own business.'),
-      (NOW(), 1, 'Graphic Design Mentorship', 'Create stunning graphics and designs.'),
-      (NOW(), 1, 'Yoga and Mindfulness Mentorship', 'Find inner peace and balance through yoga.'),
-      (NOW(), 1, 'Photography Mentorship', 'Capture amazing moments with your camera.'),
-      (NOW(), 2, 'Mathematics Tutoring', 'I can help you understand and excel in math.');
+INSERT INTO mentor_post_tb (created_at, writer_id, title, content, state) VALUES
+      (NOW(), 1, 'Teaching Programming', 'I can teach you how to code.', 'ACTIVE'),
+      (NOW(), 1, 'Art Workshop', 'Let''s create beautiful art together.', 'DONE'),
+      (NOW(), 2, 'Software Development Mentorship', 'I can mentor you in software development.', 'ACTIVE'),
+      (NOW(), 2, 'Art and Painting Mentorship', 'Learn the art of painting with me.', 'ACTIVE'),
+      (NOW(), 1, 'Web Development Mentorship', 'I can teach you web development from scratch.', 'ACTIVE'),
+      (NOW(), 2, 'Fitness and Health Mentorship', 'Get in shape and stay healthy with my guidance.', 'ACTIVE'),
+      (NOW(), 1, 'Data Science Mentorship', 'Learn data science and machine learning with me.', 'ACTIVE'),
+      (NOW(), 2, 'Music Production Mentorship', 'Produce your own music with professional tips.', 'ACTIVE'),
+      (NOW(), 2, 'Cooking and Culinary Arts Mentorship', 'Master the art of cooking and culinary skills.', 'ACTIVE'),
+      (NOW(), 2, 'Entrepreneurship Mentorship', 'Start and grow your own business.', 'ACTIVE'),
+      (NOW(), 1, 'Graphic Design Mentorship', 'Create stunning graphics and designs.', 'ACTIVE'),
+      (NOW(), 1, 'Yoga and Mindfulness Mentorship', 'Find inner peace and balance through yoga.', 'ACTIVE'),
+      (NOW(), 1, 'Photography Mentorship', 'Capture amazing moments with your camera.', 'ACTIVE'),
+      (NOW(), 2, 'Mathematics Tutoring', 'I can help you understand and excel in math.', 'ACTIVE');
 
 
 -- notConnectedRegisterUser Table
 INSERT INTO not_connected_register_user_tb (created_at, mentor_post_id, mentee_user_id, state) VALUES
       (NOW(), 1, 3, 'AWAIT'),
       (NOW(), 1, 4, 'ACCEPT'),
-      (NOW(), 2, 5, 'AWAIT');
+      (NOW(), 3, 5, 'AWAIT');
 
 -- connectedUser Table
-INSERT INTO connected_user_tb (created_at, mentor_user_id, mentee_user_id)
-VALUES
-    (NOW(), 1, 5),
-    (NOW(), 1, 6),
-    (NOW(), 2, 3);
+INSERT INTO connected_user_tb (created_at, mentor_post_id, mentee_user_id) VALUES
+    (NOW(), 2, 3),
+    (NOW(), 2, 5),
+    (NOW(), 2, 6);
 
 -- userInterest Table
 INSERT INTO user_interest_tb (created_at, user_id, interest_id) VALUES
