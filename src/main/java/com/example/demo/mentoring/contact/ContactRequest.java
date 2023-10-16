3bc238ab6b8190715e4ae2aaacde8b796cac23e3
Jin_00 <anjdal64@gmail.com>
2023-10-16 12:26:17 +0900

    feat : 멘토링 신청 수락 controller 뼈대 작성
    Signed-off-by: Jin_00 <anjdal64@gmail.com>

--- src/main/java/com/example/demo/mentoring/contact/ContactRequest.java
+++ src/main/java/com/example/demo/mentoring/contact/ContactRequest.java
@@ -1,4 +1,17 @@
 package com.example.demo.mentoring.contact;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotNull;
+
 public class ContactRequest {
+
+    @Getter @Setter
+    public static class AcceptDTO {
+        @NotNull
+        private int mentorId;
+        @NotNull
+        private int menteeId;
+    }
 }
--- src/main/java/com/example/demo/mentoring/contact/ContactRestController.java
+++ src/main/java/com/example/demo/mentoring/contact/ContactRestController.java
@@ -7,9 +7,9 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -45,5 +45,14 @@ public class ContactRestController {
         ContactResponse.postCountDTO responseDTO = contactService.postCountsByMentor(userDetails.getUser().getId());
         return ResponseEntity.ok(ApiUtils.success(responseDTO));
     }
+
+    @PostMapping(value = "/contacts/{id}/accept")
+    @Operation(summary = "멘토링 신청 수락", description = "멘토링 신청을 수락한다.")
+    public ResponseEntity<?> acceptContact(@PathVariable int id, @RequestBody @Valid ContactRequest.AcceptDTO acceptDTO, Error errors, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        // TO-DO : 멘토링 신청 수락 API 로직 만들기
+        return null;
+    }
+
+
 
 }
