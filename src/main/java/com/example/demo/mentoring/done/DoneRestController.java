ff363aa192a0736ff96a497427347079fccfa75a
Jin_00 <anjdal64@gmail.com>
2023-11-04 01:25:51 +0900

    feat : 멘티 : done 화면 조회기능 추가
    Signed-off-by: Jin_00 <anjdal64@gmail.com>

--- src/main/java/com/example/demo/mentoring/done/DoneRestController.java
+++ src/main/java/com/example/demo/mentoring/done/DoneRestController.java
@@ -2,7 +2,6 @@ package com.example.demo.mentoring.done;
 
 import com.example.demo.config.auth.CustomUserDetails;
 import com.example.demo.config.utils.ApiUtils;
-import com.example.demo.mentoring.contact.ContactResponse;
 import com.example.demo.user.Role;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -22,10 +21,10 @@ public class DoneRestController {
     @GetMapping(value = "/contacts/done")
     @Operation(summary = "contact 화면 조회", description = "멘토, 멘티 화면에 따라 적절한 화면을 보여준다.")
     public ResponseEntity<?> findAllContacts(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        // TO-DO : done 화면 조회 API 로직 만들기 ( 멘토, 멘티 나눠서 )
-        if ( userDetails.getUser().getRole() == Role.MENTEE ) {
-            return null;
+        if ( userDetails.getUser().getRole().equals(Role.MENTEE) ) {
+            List<DoneResponse.DoneDashBoardDTO> responseDTO = doneService.findByMentee(userDetails.getUser());
+            return ResponseEntity.ok(ApiUtils.success(responseDTO));
         }
-        return null;
-    }
+        List<DoneResponse.DoneDashBoardDTO> responseDTO = doneService.findByMentor(userDetails.getUser());
+        return ResponseEntity.ok(ApiUtils.success(responseDTO));
 }
