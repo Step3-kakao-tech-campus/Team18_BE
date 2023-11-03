f6c3e390da74a903c87bf5ba765ba400320c8a34
Jin_00 <anjdal64@gmail.com>
2023-11-03 16:20:59 +0900

    feat : done 화면 조회 response 작성
    Signed-off-by: Jin_00 <anjdal64@gmail.com>

--- src/main/java/com/example/demo/mentoring/done/DoneRestController.java
+++ src/main/java/com/example/demo/mentoring/done/DoneRestController.java
@@ -1,2 +1,31 @@
-package com.example.demo.mentoring.done;public class DoneRestController {
+package com.example.demo.mentoring.done;
+
+import com.example.demo.config.auth.CustomUserDetails;
+import com.example.demo.config.utils.ApiUtils;
+import com.example.demo.mentoring.contact.ContactResponse;
+import com.example.demo.user.Role;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class DoneRestController {
+
+    private final DoneService doneService;
+
+    @GetMapping(value = "/contacts/done")
+    @Operation(summary = "contact 화면 조회", description = "멘토, 멘티 화면에 따라 적절한 화면을 보여준다.")
+    public ResponseEntity<?> findAllContacts(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        // TO-DO : done 화면 조회 API 로직 만들기 ( 멘토, 멘티 나눠서 )
+        if ( userDetails.getUser().getRole() == Role.MENTEE ) {
+            return null;
+        }
+        return null;
+    }
 }
--- src/main/java/com/example/demo/mentoring/done/DoneService.java
+++ src/main/java/com/example/demo/mentoring/done/DoneService.java
@@ -1,2 +1,20 @@
-package com.example.demo.mentoring.done;public class DoneService {
+package com.example.demo.mentoring.done;
+
+import com.example.demo.mentoring.MentorPost;
+import com.example.demo.mentoring.contact.ContactResponse;
+import com.example.demo.mentoring.contact.NotConnectedRegisterUser;
+import com.example.demo.user.User;
+import com.example.demo.user.userInterest.UserInterest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class DoneService {
+
 }
