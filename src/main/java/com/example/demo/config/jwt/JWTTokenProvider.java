cbeb83a3d0d17d0b588196f8c00be519a1b46e2e
cosmos_1885 <dmsrkql104321@gmail.com>
2023-11-04 07:02:12 +0900

    feat: update JWTToken

--- src/main/java/com/example/demo/config/jwt/JWTTokenProvider.java
+++ src/main/java/com/example/demo/config/jwt/JWTTokenProvider.java
@@ -1,36 +1,128 @@
 package com.example.demo.config.jwt;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.SignatureVerificationException;
-import com.auth0.jwt.exceptions.TokenExpiredException;
-import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.demo.config.auth.CustomUserDetailService;
+import com.example.demo.config.auth.CustomUserDetails;
+import com.example.demo.config.errors.exception.Exception401;
 import com.example.demo.user.User;
+import com.example.demo.refreshToken.RefreshToken;
+import com.example.demo.refreshToken.RefreshTokenJPARepository;
+import com.example.demo.refreshToken.TokenResponse;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.Optional;
 
+@RequiredArgsConstructor
 @Component
 public class JWTTokenProvider {
-    public static final String SECRET = "SecretKey";
-    public static final Long EXP = 1000L * 60 * 30; // 30분
-    public static final String HEADER = "Authorization";
-    public static final String TOKEN_PREFIX = "Bearer "; // 스페이스 필요함
 
-    public static String create(User account) {
-        String jwt = JWT.create()
-                .withSubject(account.getEmail())
-                .withExpiresAt(new Date(System.currentTimeMillis() + EXP))
-                .withClaim("user_id", account.getId())
-                .withClaim("user_email", account.getEmail())
-                .sign(Algorithm.HMAC512(SECRET));
-        return TOKEN_PREFIX + jwt;
+    public static final String Header = "Authorization";
+    public static final String Token_Prefix = "Bearer ";
+
+    public static final String SecretKey = "a2FrYW8tdGVjaC1jYW1wdXMtcHJvamVjdC1nYXJkZW4tc3ByaW5nLXNlY3VyaXR5LWp3dC10b2tlbi1zZWNyZXQta2V5";
+    public static final int AccessTokenValidTime = 1000 * 60 * 2;
+    public static final int RefreshTokenValidTime = 1000 * 60 * 5;
+
+    private final CustomUserDetailService userDetailService;
+    private final RefreshTokenJPARepository refreshTokenJPARepository;
+
+    public static TokenResponse.TokenDTO createToken(User user) {
+        String accessToken = createAccessToken(user);
+        String refreshToken = createRefreshToken(user);
+        return new TokenResponse.TokenDTO(accessToken, refreshToken);
     }
 
-    public static DecodedJWT verify(String jwt) throws SignatureVerificationException, TokenExpiredException {
-        jwt = jwt.replace(JWTTokenProvider.TOKEN_PREFIX, "");
-        DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512(SECRET))
-                .build().verify(jwt);
-        return decodedJWT;
+    public static String createAccessToken(User user) {
+        String accessToken = Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("id", user.getId())
+                .claim("firstName", user.getFirstName())
+                .claim("lastName", user.getLastName())
+                .claim("email", user.getEmail())
+                .claim("country", user.getCountry())
+                .claim("introduction", user.getIntroduction())
+                .claim("age", user.getAge())
+                .claim("phone", user.getPhone())
+                .claim("profileImage", user.getProfileImage())
+                .claim("role", user.getRole())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + AccessTokenValidTime))
+                .signWith(SignatureAlgorithm.HS512, SecretKey)
+                .compact();
+        return accessToken;
     }
+
+    public static String createRefreshToken(User user) {
+        String refreshToken = Jwts.builder()
+                .setSubject(user.getEmail())
+                .claim("id", user.getId())
+                .claim("email", user.getEmail())
+                .claim("role", user.getRole())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + RefreshTokenValidTime))
+                .signWith(SignatureAlgorithm.HS512, SecretKey)
+                .compact();
+        return refreshToken;
+    }
+
+    public Claims decodeJwtToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(SecretKey)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return claims;
+    }
+
+    // JWT 토큰 복호화 -> 인증 정보 조회
+    public Authentication getAuthentication(String accessToken) {
+        Claims claims = decodeJwtToken(accessToken);
+        CustomUserDetails userDetails = userDetailService.loadUserByUsername(claims.getSubject());
+        return new UsernamePasswordAuthenticationToken(userDetails, userDetails.getPassword(), userDetails.getAuthorities());
+    }
+
+    public static boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(SecretKey)
+                    .build()
+                    .parseClaimsJws(token);
+            return true;
+        } catch (SignatureException e) {
+            System.out.println("Invalid JWT Signature.");
+        } catch (MalformedJwtException e) {
+            System.out.println("Invalid JWT Token.");
+        } catch (ExpiredJwtException e) {
+            System.out.println("Expired JWT Token.");
+        } catch (UnsupportedJwtException e) {
+            System.out.println("Unsupported JWT Token.");
+        } catch (IllegalArgumentException e) {
+            System.out.println("JWT claims string is empty.");
+        }
+        return false;
+    }
+
+//    public static boolean validateRefreshToken(String token) {
+//        try {
+//            Jwts.parserBuilder()
+//                    .setSigningKey(SecretKey)
+//                    .build()
+//                    .parseClaimsJws(token);
+//            return true;
+//        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+//            System.out.println("Invalid JWT Token");
+//        } catch (ExpiredJwtException e) {
+//            System.out.println("Expired JWT Token");
+//        } catch (UnsupportedJwtException e) {
+//            System.out.println("Unsupported JWT Token");
+//        } catch (IllegalArgumentException e) {
+//            System.out.println("JWT claims string is empty.");
+//        }
+//        return false;
+//    }
 }
