21a9f64572d8859005232498857149365be0db7e
cosmos_1885 <dmsrkql104321@gmail.com>
2023-11-04 07:03:59 +0900

    feat: update User

--- src/main/java/com/example/demo/user/UserRestController.java
+++ src/main/java/com/example/demo/user/UserRestController.java
@@ -1,19 +1,23 @@
 package com.example.demo.user;
 
+import com.example.demo.config.auth.CustomUserDetails;
 import com.example.demo.config.jwt.JWTTokenProvider;
-import com.example.demo.config.utils.ApiUtils;
+import com.example.demo.config.utils.ApiResponseBuilder;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.models.Response;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.UnsupportedEncodingException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 @Api(tags = "User API")
 @RequiredArgsConstructor
@@ -26,34 +30,47 @@ public class UserRestController {
     @PostMapping(value = "/users/emailcheck")
     public ResponseEntity<?> emailCheck(@RequestBody @Valid UserRequest.EmailCheckDTO requestDTO, Errors errors) {
         userService.emailCheck(requestDTO);
-        return ResponseEntity.status(HttpStatus.OK).body(ApiUtils.successWithNoContent());
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponseBuilder.successWithNoContent());
     }
 
     @Operation(summary = "회원가입", description = "회원가입")
     @PostMapping(value = "/users/signup")
     public ResponseEntity<?> signup(@RequestBody @Valid UserRequest.SignUpDTO requestDTO, Errors errors) {
         userService.signup(requestDTO);
-        return ResponseEntity.status(HttpStatus.OK).body(ApiUtils.successWithNoContent());
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponseBuilder.successWithNoContent());
     }
 
     @Operation(summary = "로그인", description = "로그인")
     @PostMapping(value = "/users/login")
-    public ResponseEntity<?> login(@RequestBody @Valid UserRequest.LoginDTO requestDTO, Errors errors) {
+    public ResponseEntity<?> login(@RequestBody @Valid UserRequest.LoginDTO requestDTO, Errors errors, HttpServletResponse httpServletResponse) throws UnsupportedEncodingException {
         UserResponse.LoginDTO responseDTO = userService.login(requestDTO);
-        return ResponseEntity.status(HttpStatus.OK).header(JWTTokenProvider.HEADER, responseDTO.getJWTToken()).body(ApiUtils.success(responseDTO.getUserDetailDTO()));
+
+        String accessToken = JWTTokenProvider.Token_Prefix + responseDTO.getJwtToken().getAccessToken();
+        String refreshToken = JWTTokenProvider.Token_Prefix + responseDTO.getJwtToken().getRefreshToken();
+        refreshToken = URLEncoder.encode(refreshToken, "utf-8");
+
+        httpServletResponse.addHeader(JWTTokenProvider.Header, accessToken);
+        Cookie cookie = new Cookie("RefreshToken", refreshToken);
+        cookie.setHttpOnly(true);
+        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(60 * 5);
+        httpServletResponse.addCookie(cookie);
+
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponseBuilder.success(responseDTO.getUserDetailDTO()));
     }
 
     @Operation(summary = "마이 페이지 프로필 확인", description = "마이 페이지에서 프로필 확인")
-    @GetMapping(value = "/profiles/{id}")
-    public ResponseEntity<?> profile(@PathVariable int id) {
-        UserResponse.ProfileDTO responseDTO = userService.findProfile(id);
-        return ResponseEntity.status(HttpStatus.OK).body(ApiUtils.success(responseDTO));
+    @GetMapping(value =  {"/profiles", "/profiles/{id}"})
+    public ResponseEntity<?> profile(@PathVariable(required = false) Integer id, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        UserResponse.ProfileDTO responseDTO = userService.findProfile(id, userDetails.getUser());
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponseBuilder.success(responseDTO));
     }
     
     @Operation(summary = "마이 페이지 프로필 수정", description = "마이 페이지에서 프로필 수정")
-    @PutMapping(value = "/profiles/{id}")
-    public ResponseEntity<?> profileUpdate(@PathVariable int id, @RequestBody @Valid UserRequest.ProfileUpdateDTO requestDTO, Errors errors) {
-        UserResponse.ProfileDTO responseDTO = userService.updateProfile(id, requestDTO);
-        return ResponseEntity.status(HttpStatus.OK).body(ApiUtils.success(responseDTO));
+    @PutMapping(value = "/profiles")
+    public ResponseEntity<?> profileUpdate(@RequestBody @Valid UserRequest.ProfileUpdateDTO requestDTO, Errors errors, @AuthenticationPrincipal CustomUserDetails userDetails) {
+        UserResponse.ProfileDTO responseDTO = userService.updateProfile(userDetails.getUser().getId(), requestDTO);
+        return ResponseEntity.status(HttpStatus.OK).body(ApiResponseBuilder.success(responseDTO));
     }
 }
