84639e8b6037766c886726b41690aa58cc7f0b29
sjmjys954646 <sjmjys954646@gmail.com>
2023-11-05 00:57:24 +0900

    feat : random 4 usercategory native query

--- src/main/java/com/example/demo/video/VideoJPARepository.java
+++ src/main/java/com/example/demo/video/VideoJPARepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,6 +14,17 @@ import java.util.Optional;
 public interface VideoJPARepository extends JpaRepository<Video, Integer> {
     Optional<Video> findById(int id);
 
-    @Query("select v from Video v join fetch VideoInterest vi on v.id = vi.video.id where vi.interest.category in :userInterest")
-    Page<Video> findByVideoCategory(List<String> userInterests, Pageable pageable);
+    @Query(value = "SELECT * FROM videos v " +
+            "INNER JOIN video_interests vi ON v.id = vi.video_id " +
+            "INNER JOIN interest_tb i ON vi.interest_id = i.id " +
+            "WHERE i.category IN :userInterests " +
+            "ORDER BY RAND()", // MySQL에서 랜덤 정렬
+            countQuery = "SELECT COUNT(*) FROM videos v " +
+                    "INNER JOIN video_interests vi ON v.id = vi.video_id " +
+                    "INNER JOIN interest_tb i ON vi.interest_id = i.id " +
+                    "WHERE i.category IN :userInterests", // 페이징을 위한 count 쿼리
+            nativeQuery = true)
+    Page<Video> findByVideoCategory(@Param("userInterests") List<String> userInterests, Pageable pageable);
+
+
 }
