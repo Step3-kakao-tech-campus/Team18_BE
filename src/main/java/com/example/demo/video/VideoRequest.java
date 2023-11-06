87b62191b48121eb769c2484fa6e5ea27c3ec839
sjmjys954646 <sjmjys954646@gmail.com>
2023-11-06 16:35:35 +0900

    feat : VideoPostRequest DTO post요청 DTO

--- src/main/java/com/example/demo/video/VideoRequest.java
+++ src/main/java/com/example/demo/video/VideoRequest.java
@@ -1,4 +1,32 @@
 package com.example.demo.video;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.Column;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
 public class VideoRequest {
+    @Getter
+    public static class CreateDTO {
+        private String videoUrl;
+        private String videoTitleKorean;
+        private String videoTitleEng;
+        private String videoStartTime;
+        private String videoEndTime;
+        private String videoThumbnailUrl;
+        private List<SubtitleCreateDTO> subtitleCreateDTOList;
+
+        @Getter
+        public static class SubtitleCreateDTO{
+            private String korSubtitleContent;
+            private String engSubtitleContent;
+            private String korStartTime;
+            private String engStartTime;
+            private String korEndTime;
+            private String engEndTime;
+        }
+    }
 }
